75eabb95d7e983d6198a85a862f87cff8a9a1e02
AvishMD <avishmd2007@gmail.com>
2026-02-11 15:51:34 +0530

    Create p13.c

---
+++ p13.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char a;
+    scanf("%c", &a);
+    if(a >= 65 && a <= 90)
+        printf("Uppercase Letter");
+    else
+        printf("Not Uppercase Letter");
+    
+}

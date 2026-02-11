64a5f95397d1d40c7f968e18eed7b5bf8deb8503
AvishMD <avishmd2007@gmail.com>
2026-02-11 10:34:17 +0530

    Create p4.c

---
+++ p4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a;
+    int b;
+    scanf("%d %d",&a,&b);
+    if(a>b)
+        printf("%d Greater than", a);
+    else 
+        printf("%d Greater than", b);
+
+}

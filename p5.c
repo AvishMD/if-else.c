bec36ce65f99654f339aa09dc76eb41675650764
AvishMD <avishmd2007@gmail.com>
2026-02-11 11:08:02 +0530

    Create p5.c

---
+++ p5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    if(a%100==0)
+        printf("Not Leap Year");
+    if(a%400==0 || a%4==0)
+        printf("Leap Year");
+    else
+        printf("Not a Leap year");
+        
+    }
+    
+

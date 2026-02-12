0c5151abfa45b30faa26ec4d36a3457ee5e06d56
AvishMD <avishmd2007@gmail.com>
2026-02-12 09:55:25 +0530

    Create p17.c

---
+++ p17.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    scanf("%d", &a);
+    if(a>=10 && a<=20)
+        printf("Within the range");
+    else
+        printf("Not within the range");
+    return 0;        
+
+}
